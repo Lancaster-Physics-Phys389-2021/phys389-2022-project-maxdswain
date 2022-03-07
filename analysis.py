4100719a0b0d224c277cccebc51c2f1e4c340179
maxdswain <maxdswain@btinternet.com>
2022-03-07 14:03:45 +0000

    Improved animation
    - Animation is now fixed and a lot smoother, looks to be running at 60fps instead of stuttering like it did previously.

--- analysis.py
+++ analysis.py
@@ -5,9 +5,9 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 from itertools import product, combinations
 
-def animation_frame(iteration, df, scatters):
+def animation_frame(iteration, df, scatters, k):
     for i in range(df["Position"][0].shape[0]):
-        scatters[i]._offsets3d=(df["Position"][iteration*10][i, 0:1], df["Position"][iteration*10][i, 1:2], df["Position"][iteration*10][i, 2:])
+        scatters[i]._offsets3d=(df["Position"][iteration*k][i, 0:1], df["Position"][iteration*k][i, 1:2], df["Position"][iteration*k][i, 2:])
     return scatters
 
 class Analysis:
@@ -16,7 +16,7 @@ class Analysis:
         self.df=pd.read_pickle("Simulation_Data.csv")
 
     def animate(self):
-        iterations=int(self.df.shape[0]/10)
+        iterations=int(self.df.shape[0]/100)
         fig=plt.figure()
         ax=p3.Axes3D(fig)
         scatters=[ax.scatter(self.df["Position"][0][i][0], self.df["Position"][0][i][1], self.df["Position"][0][i][2], c="black") for i in range(self.df["Position"][0].shape[0])]
@@ -28,7 +28,7 @@ class Analysis:
         ax.set_ylabel("y position")
         ax.set_zlabel("z position")
         ax.view_init(25, 10)
-        ani=animation.FuncAnimation(fig, animation_frame, iterations, fargs=(self.df, scatters), interval=50, blit=False, repeat=True)
+        ani=animation.FuncAnimation(fig, animation_frame, iterations, fargs=(self.df, scatters, 100), blit=False, repeat=True)
         #writer=animation.FFMpegWriter(fps=60)
         writer=animation.PillowWriter(fps=60) 
         ani.save("animation.gif", writer=writer)
