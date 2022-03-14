a7731d867d11d84fdd8a1cc14a01d3204e2683f2
Max Swain <89113255+maxdswain@users.noreply.github.com>
2022-03-14 14:29:23 +0000

    Large code optimisations
    - Added jit compiler for uniform angle generation and particle collision detection and calculation using the numba library.
    - This involved re-writing parts of both methods as well as making a function for seeding random number generation in numba.
    - Fixed spelling mistakes in variable names and updated comments.

--- montecarlosim.py
+++ montecarlosim.py
@@ -1,13 +1,18 @@
 import numpy as np
+from numba import njit
+from itertools import product, combinations
 import pandas as pd
+from copy import deepcopy
 from scipy.stats import maxwell
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from itertools import product, combinations
-from copy import deepcopy
 import json
 
-#check conserved quantities are conserved; detailed quality comments and docstrings
+@njit
+def set_seed(value):
+    np.random.seed(value)
+
+#check conserved quantities are conserved; detailed quality comments and docstrings; set suitable initial conditions and start getting results
 class Simulation:
 
     def __init__(self):
@@ -20,28 +25,31 @@ class Simulation:
         self.length=config["Length of Box"] #length of the sides of the box in pm
         self.k=config["Boltzmann Constant"] #Boltzmann constant
         self.m=config["Mass"] #mass of one molecule of oxygen in kg
-        self.effectiveDiamter=config["Effective Diameter"] #effective diameter of oxygen in pm
+        self.effectiveDiameter=config["Effective Diameter"] #effective diameter of oxygen in pm
         self.walls=config["Walls"] #list of 6 walls 0 - periodic, 1 - specular, 2 - thermal; check folder for cube with labelled faces, list is in ascending order of index.
         self.Ne=1*self.N #number of effective particles
         self.numberDensity=self.N/(self.length)**3
         self.rng=np.random.default_rng(seed=11)
+        set_seed(11)
         deltaZ=np.max([num for num in range(1, self.length) if self.length%num==0 and num<self.meanPathLength()])
-        self.cells=[[deltaZ*i, (i+1)*deltaZ] for i in range(int(self.length/deltaZ))]
+        self.cells=np.array([[deltaZ*i, (i+1)*deltaZ] for i in range(int(self.length/deltaZ))])
         self.cellVolume=deltaZ*self.length**2
 
-    def uniformAngleGeneration(self):
-        azimuthal=2*np.pi*self.rng.random(None)
-        q=2*self.rng.random(None)-1
+    @staticmethod
+    @njit
+    def uniformAngleGeneration():
+        azimuthal=2*np.pi*np.random.random()
+        q=2*np.random.random()-1
         cosTheta, sinTheta=q, np.sqrt(1-q**2)
         return np.array([sinTheta*np.cos(azimuthal), sinTheta*np.sin(azimuthal), cosTheta])
 
     def randomGeneration(self):
         self.positions=self.rng.integers(low=0, high=self.length+1, size=(self.N, 3)).astype(float) #randomly generated positions of N particles in pm
-        self.speeds=maxwell.rvs(scale=5, size=(self.N, 1), random_state=11) #velocities randomly generated using Maxwell distribution - adjust scale as appropriate to adjust speeds
-        self.velocities=np.array([self.speeds[i][0]*self.uniformAngleGeneration() for i in range(self.N)]).reshape(self.N, 3)
+        self.speeds=maxwell.rvs(scale=5, size=self.N, random_state=11) #velocities randomly generated using Maxwell distribution - adjust scale as appropriate to adjust speeds
+        self.velocities=np.array([self.speeds[i]*self.uniformAngleGeneration() for i in range(self.N)]).reshape(self.N, 3)
 
     def meanPathLength(self):
-        return 1/(np.sqrt(2)*np.pi*(self.effectiveDiamter**2)*self.numberDensity)
+        return 1/(np.sqrt(2)*np.pi*(self.effectiveDiameter**2)*self.numberDensity)
 
     def update(self):
         self.positions+=np.dot(self.velocities, self.dT)
@@ -54,23 +62,25 @@ class Simulation:
         for i, j in np.argwhere(self.positions >= self.length):
             callWallCollision[self.walls[j*2+1]]([i, j])
 
-    def particle_collision_detection(self):
-        for cell in self.cells:
-            posZ=self.positions[:, 2] #taking only z components as cells are divided in z axis
+    @staticmethod
+    @njit
+    def particle_collision_detection(cells, positions, effectiveDiameter, Ne, dT, cellVolume, velocities, uniformAngleGeneration):
+        for cell in cells:
+            posZ=positions[:, 2] #taking only z components as cells are divided in z axis
             particlesInCell=np.argwhere((posZ>=cell[0]) & (posZ<cell[1]))
             numberOfParticlesInCell=len(particlesInCell)
             if numberOfParticlesInCell<2: continue
             velMax=25 #chosen by calculating the velocity difference between particles then looking at the maxes of that over numerous iterations - is an overestimate
-            numberOfCollisions=np.rint(numberOfParticlesInCell**2*np.pi*self.effectiveDiamter**2*velMax*self.Ne*self.dT/(2*self.cellVolume)).astype(int)
+            numberOfCollisions=int(np.rint(numberOfParticlesInCell**2*np.pi*effectiveDiameter**2*velMax*Ne*dT/(2*cellVolume)))
             for x in range(numberOfCollisions):
-                randomParticles=[particlesInCell[self.rng.integers(numberOfParticlesInCell)], particlesInCell[self.rng.integers(numberOfParticlesInCell)]] #need to prevent it from randomly selecting the same particle (chance of happening in cells with low number of particles)
-                norm=np.linalg.norm(self.velocities[randomParticles[0][0]]-self.velocities[randomParticles[1][0]])
-                condition=norm/velMax
-                if condition>self.rng.random(1):
-                    velCM=0.5*np.array(self.velocities[randomParticles[0][0]]+self.velocities[randomParticles[1][0]])
-                    velR=norm*self.uniformAngleGeneration()
-                    self.velocities[randomParticles[0][0]]=velCM+0.5*velR
-                    self.velocities[randomParticles[1][0]]=velCM-0.5*velR
+                randomParticlesOne, randomParticleTwo=particlesInCell[np.random.randint(numberOfParticlesInCell)], particlesInCell[np.random.randint(numberOfParticlesInCell)]
+                norm=np.linalg.norm(velocities[randomParticlesOne[0]]-velocities[randomParticleTwo[0]])
+                if norm/velMax>np.random.random():
+                    velCM=0.5*(velocities[randomParticlesOne[0]]+velocities[randomParticleTwo[0]])
+                    velR=norm*uniformAngleGeneration()
+                    velocities[randomParticlesOne[0]]=velCM+0.5*velR
+                    velocities[randomParticleTwo[0]]=velCM-0.5*velR
+        return velocities
 
     def run(self):
         self.randomGeneration()
@@ -78,7 +88,15 @@ class Simulation:
         for x in range(self.timeIntervals):
             self.update()
             self.wall_collision_detection()
-            self.particle_collision_detection()
+            self.velocities=self.particle_collision_detection(self.cells, 
+                self.positions, 
+                self.effectiveDiameter,
+                self.Ne, 
+                self.dT, 
+                self.cellVolume, 
+                self.velocities, 
+                self.uniformAngleGeneration
+                )
             tempPos.append(deepcopy(self.positions))
             tempVel.append(deepcopy(self.velocities))
         self.time=[i*self.dT for i in range(self.timeIntervals+1)]
